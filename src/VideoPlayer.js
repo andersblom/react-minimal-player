527d162bb870542777a85dae245ce3aba674fc3a
Anders Blom <anders@andersblom.dk>
2018-11-11 19:02:47 -0600

    Current play state in UI + Moved video el acquisition into method

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -7,14 +7,20 @@ export default class VideoPlayer extends Component {
     playing: false,
   }
 
+  getVideoElement = () => {
+    const videoElement = document.querySelector(VIDEO_EL_ID);
+    console.log(videoElement.duration, videoElement.currentTime)
+    return videoElement;
+  }
+
   playVideo = () => {
-    const el = document.querySelector(VIDEO_EL_ID);
+    const el = this.getVideoElement();
     el.play();
     this.setState({ playing: true });
   }
 
   pauseVideo = () => {
-    const el = document.querySelector(VIDEO_EL_ID);
+    const el = this.getVideoElement();
     el.pause();
     this.setState({ playing: false });
   }
@@ -29,9 +35,14 @@ export default class VideoPlayer extends Component {
   render() {
     return (
       <div>
-        <video id="rv-video-element" onClick={this.playPause}>
+        <video id="rv-video-element">
           <source src="https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_5mb.mp4" />
         </video>
+        <div>
+          <button onClick={this.playPause}>
+            {this.state.playing ? 'pause' : 'play'}
+          </button>
+        </div>
       </div>
     )
   }
