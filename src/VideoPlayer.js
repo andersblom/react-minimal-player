92087424b889c4ad626b4b2762703472df940d4f
Anders Blom <anders@andersblom.dk>
2018-11-11 18:56:23 -0600

    play/pause/playpause methods basics

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
 
+const VIDEO_EL_ID = '#rv-video-element';
+
 export default class VideoPlayer extends Component {
   state = {
     playing: false,
   }
 
   playVideo = () => {
-    const el = document.querySelector('#video');
-    console.log(this.state.playing);
-
-    if (this.state.playing) {
-      el.pause();
-      return this.setState({ playing: false });
-    }
-
+    const el = document.querySelector(VIDEO_EL_ID);
     el.play();
-      return this.setState({ playing: true });
+    this.setState({ playing: true });
+  }
+
+  pauseVideo = () => {
+    const el = document.querySelector(VIDEO_EL_ID);
+    el.pause();
+    this.setState({ playing: false });
+  }
+
+  playPause = () => {
+    if (this.state.playing) {
+      return this.pauseVideo();
+    }
+    return this.playVideo();
   }
 
   render() {
     return (
       <div>
-        <video id="video" onClick={this.playVideo}>
+        <video id="rv-video-element" onClick={this.playPause}>
           <source src="https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_5mb.mp4" />
         </video>
       </div>
