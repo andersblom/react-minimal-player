c7669c03c56f0019726e8014fed0113b146b4b02
Anders Blom <anders@andersblom.dk>
2018-11-16 21:53:03 -0600

    Deleted playbutton code, needs work

--- src/Controls/PlayPauseButton/styles.js
+++ src/Controls/PlayPauseButton/styles.js
@@ -8,19 +8,5 @@ export const ButtonContainer = styled.button`
 `;
 
 export const ButtonIcon = styled.div`
-  box-sizing: border-box;
-  height: 10px;
-  
-  border-color: transparent transparent transparent #202020;
-  transition: 100ms all ease;
-  will-change: border-width;
-  cursor: pointer;
 
-  border-style: solid;
-  border-width: 10px 0 10px 4px;   
-
-  ${props => !props.playing && css`
-    border-style: double;
-    border-width: 0px 0 0px 4px;
-  `}
 `;
