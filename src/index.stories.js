18dd1d4d80b4210bf9336462565b2ea92eef249c
Anders Blom <anders@andersblom.dk>
2018-11-16 20:39:35 -0600

    Story/file structure changes

--- src/index.stories.js
+++ src/index.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 
-import VideoPlayer from './VideoPlayer';
+import VideoPlayer from './index';
 
 storiesOf('VideoPlayer', module)
   .add('Default', () => <VideoPlayer width="500px" />);
