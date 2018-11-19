db0497975669c7a35db405637c0fd0941dd03b95
Anders Blom <anders@andersblom.dk>
2018-11-19 16:39:59 -0600

    Play/pause icon button

--- src/Controls/PlayPauseButton/styles.js
+++ src/Controls/PlayPauseButton/styles.js
@@ -5,8 +5,20 @@ export const ButtonContainer = styled.button`
   box-shadow: none;
   border: none;
   padding: 0;
+  background-color: transparent;
 `;
 
 export const ButtonIcon = styled.div`
+  /* https://css-tricks.com/making-pure-css-playpause-button/ */
+  width: 16px;
+  height: 16px;
+  border-style: solid;
+  box-sizing: border-box;
+  border-width: 8px 0px 8px 14px;
+  border-color: transparent transparent transparent #ffffff;
 
+ ${props => props.playing && css`
+    border-style: double;
+    border-width: 0px 0px 0px 10px;
+ `}
 `;
